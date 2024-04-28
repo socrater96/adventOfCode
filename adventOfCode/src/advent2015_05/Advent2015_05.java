1927fbfff3fa999a1f9d1a67d74aabc740239803
nicor <nicor@nico-pc>
2024-04-28 12:29:10 +0200

    terminado dia 5 parte 1

--- adventOfCode/src/advent2015_05/Advent2015_05.java
+++ adventOfCode/src/advent2015_05/Advent2015_05.java
@@ -1,13 +1,11 @@
 package advent2015_05;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Advent2015_05 {
-	static boolean condicion1(String linea) {
+	static boolean condicion1(String linea) {//que haya al menos tres vocales en el String
 		int cc=0;
 		for(int i=0;i<linea.length();i++) {
 			if(linea.charAt(i)=='a'||linea.charAt(i)=='e'||linea.charAt(i)=='i'||linea.charAt(i)=='o'||linea.charAt(i)=='u') {
@@ -18,39 +16,33 @@ public class Advent2015_05 {
 		}
 		return false;
 	}
-	static boolean condicion2(String linea) {
+	static boolean condicion2(String linea) {//que haya al menos una letra que se repita en el caracter siguiente
 		for(int i=0;i<linea.length()-1;i++) {
-			for(int j=1;j<linea.length();j++) {
-				if(linea.charAt(i)==linea.charAt(j)) {
-					return true;
-				}
+			if(linea.charAt(i)==linea.charAt(i+1))
+				return true;
 			}
-		}
-		return true;
+		return false;
 	}
-	static boolean condicion3(String linea) {
+	static boolean condicion3(String linea) {//que no se den las combinaciones "ab", "cd","pq" o "xy"
 			for(int i=0;i<linea.length()-1;i++) {
-				for(int j=1;j<linea.length();j++) {
-					if(linea.charAt(i)=='a' && linea.charAt(j)=='b'||
-						linea.charAt(i)=='c' && linea.charAt(j)=='d'||
-						linea.charAt(i)=='p' && linea.charAt(j)=='q'||
-						linea.charAt(i)=='x' && linea.charAt(j)=='y') {
-						return false;
-					}
-				}	
-			}
+				if(linea.charAt(i)=='a' && linea.charAt(i+1)=='b'||
+					linea.charAt(i)=='c' && linea.charAt(i+1)=='d'||
+					linea.charAt(i)=='p' && linea.charAt(i+1)=='q'||
+					linea.charAt(i)=='x' && linea.charAt(i+1)=='y') {
+					return false;
+				}
+			}	
 		return true;
 	}
 	public static void main(String [] args) {
 		int cc=0;
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("stringsBonitos.txt"));
-			boolean condicion=false;
-			String linea="";
-			
+			BufferedReader br = new BufferedReader(new FileReader("cadenas.txt"));
+			String linea="";	
 			while((linea=br.readLine())!=null) {
 				if(condicion1(linea)&&condicion2(linea)&&condicion3(linea)) {
 					cc++;
+					System.out.println(linea+"\t"+cc);
 				}
 			}
 		}catch(IOException e) {}
